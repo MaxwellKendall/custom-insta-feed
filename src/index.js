3d16ff28b2c48da3ade81981fe9269ddc2c5e724
Maxwell Kendall <maxwellkendall@Maxwells-MacBook-Pro.local>
2019-02-09 16:47:38 -0500

    imgs are rendering as expected

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 
 import { getUserInfo } from './api';
+
+import 'pure-react-carousel/dist/react-carousel.es.css';
 import "./styles.css";
 
 class App extends Component {
@@ -13,32 +16,76 @@ class App extends Component {
       comments: [],
       nextUrl: null
     }
+
+    this.parseResults = this.parseResults.bind(this)
+    this.renderSlides = this.renderSlides.bind(this)
   }
 
   componentDidMount(){
-    axios.get(getUserInfo)
+    // axios.get(getUserInfo)
+    axios.get('mock.json')
       .then(resp => {
-        console.log(resp)
         this.parseResults(resp.data) // 20 pictures
       })
       .catch(err => this.parseResults(err));
   }
 
   parseResults(results) {
-    const pictures = results.data;
-    const nextUrl = results.pagination.next_url;
+    const { data, pagination } = results;
+    // const nextUrl = results.pagination.next_url;
 
-    console.log(pictures[1])
-    console.log(nextUrl);
+    // console.log('data', pictures[1])
+    // console.log(nextUrl);
+    data.map((pic) => {
+      console.log('width', pic.images.low_resolution.width)
+      console.log('height', pic.images.low_resolution.height)
+    });
+    this.setState({ pix: data, nextUrl: pagination.next_url })
+  }
+
+  renderSlides() {
+    if (this.state.pix.length > 0){
+      return this.state.pix.map((pic, index) => {
+        return (
+          <Slide
+            index={index}
+            innerTag="div"
+            innerClassName="slide-parent"
+            tag="li">
+            <div className="slide">
+              <img src={pic.images.low_resolution.url} alt=""/>
+            </div>
+          </Slide>
+        );
+      });
+    }
   }
 
   render() {
     return (
-      <React.Fragment>
-        WUTUP CLAIRE
-      </React.Fragment>
+      <div className="container">
+        <CarouselProvider
+          naturalSlideWidth={293}
+          naturalSlideHeight={293}
+          visibleSlides={4}
+          // isPlaying
+          totalSlides={this.state.pix.length}>
+          <ButtonNext>
+            <p>Next</p>
+          </ButtonNext>
+          <Slider>
+            {this.renderSlides()}
+          </Slider>
+          <ButtonBack>
+            <p>Back</p>
+          </ButtonBack>
+        </CarouselProvider>
+      </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("maxs-insta-feed"));
+ReactDOM.render(
+  <App />,
+  document.getElementById("maxs-insta-feed")
+);
